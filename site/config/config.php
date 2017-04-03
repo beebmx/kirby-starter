204c886bc7b53e3bfd7f46cc70524268222fbab4
Fernando Gutierrez <fernando@beeb.mx>
2017-04-03 15:23:09 -0500

    Update kirby version

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,6 @@
 <?php
-//c::set('panel.install', false);
+//c::set('panel.install', true);
 c::set('license', env('KIRBY_LICENSE', 'put your license in here'));
 c::set('email.from', 'contacto@domain.com');
 c::set('email.subject', 'Contacto via formulario DOMAIN');
+//c::set('debug', true);
