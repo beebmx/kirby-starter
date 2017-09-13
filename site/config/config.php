4cd624cc3026bd88d6f8519c13099f6aaec3b7ed
Fernando Gutierrez <fernando@beeb.mx>
2017-09-13 16:23:46 -0500

    Added env variables to config

--- site/config/config.php
+++ site/config/config.php
@@ -1,6 +1,6 @@
 <?php
-//c::set('panel.install', true);
+c::set('panel.install', env('KIRBY_PANEL_INSTALL', false));
 c::set('license', env('KIRBY_LICENSE', 'put your license in here'));
 c::set('email.from', 'contacto@domain.com');
 c::set('email.subject', 'Contacto via formulario DOMAIN');
-//c::set('debug', true);
+c::set('debug', env('KIRBY_DEBUG', false));
